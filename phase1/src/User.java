a1ab604cea17ff02ca6d88ef2a747b95df4070c6
chanho31 <willyc.chan@mail.utoronto.ca>
2019-03-04 14:06:15 -0500

    Fixed typo

--- phase1/src/User.java
+++ phase1/src/User.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class User {
 
-    private ArrayList<Accounts> accounts = new ArrayList<Account>;
+    private ArrayList<Accounts> accounts = new ArrayList<Accounts>;
 
 
 

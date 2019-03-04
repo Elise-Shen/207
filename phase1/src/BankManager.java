9b230c6fe75718fbe166a574661ebcd1db6cb6f9
chanho31 <willyc.chan@mail.utoronto.ca>
2019-03-04 14:05:53 -0500

    Added list of accounts for User.java Added lits of users and creatAccount() for BankManager

--- phase1/src/BankManager.java
+++ phase1/src/BankManager.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+
 public class BankManager {
+
+    private ArrayList<User>  users = new ArrayList<User>;
+    private AccountFactory accountFactory = new AccountFactory();
+
+
+
+
+
+    public void createAccount(User user, String type) {
+        if (approveAccount()){
+            Accounts newAcc = accountFactory.create(type);
+            user.addAcc(newAcc);
+
+
+        }
+    }
+
+    private boolean approveAccount(){return true}
+
+
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
 }
--- phase1/src/User.java
+++ phase1/src/User.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class User {
+
+    private ArrayList<Accounts> accounts = new ArrayList<Account>;
+
+
+
+    public addAcc(Accounts account){
+        accounts.add(account);
+
+    }
 }

cac2e3465cc37518da9a86e2838342a97200441a
maoxiaoy <xiaoyuan.mao@mail.utoronto.ca>
2019-03-05 17:11:14 -0500

    implement ViewDateOfCreation

--- phase1/src/Actions/ViewCreationDate.java
+++ phase1/src/Actions/ViewCreationDate.java
@@ -1,4 +1,20 @@
 package Actions;
 
-public class ViewCreationDate {
+import Accounts.Account;
+
+public class ViewCreationDate extends UserActions {
+
+    private Account account;
+
+    public ViewCreationDate(Account account){
+        this.account = account;
+    }
+
+    /**
+     * This method print out the date of creation of an account.
+     */
+    @Override
+    public void execute() {
+        System.out.println("Account ID: " + account.getID() + " Date of Creation: " + account.getDateOfCreation());
+    }
 }

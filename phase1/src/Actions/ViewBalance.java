083df7fa904cb109f1a272f0df4e26f2a4be9ea0
maoxiaoy <xiaoyuan.mao@mail.utoronto.ca>
2019-03-05 15:32:16 -0500

    implement ViewBalance

--- phase1/src/Actions/ViewBalance.java
+++ phase1/src/Actions/ViewBalance.java
@@ -1,14 +1,22 @@
 package Actions;
 
-import Actions.Transactions;
+import Accounts.*;
 
-public class ViewBalance extends Transactions {
+public class ViewBalance extends UserActions {
 
-    public ViewBalance(){
+    private Account account;
 
+    public ViewBalance(Account account){
+        this.account = account;
     }
 
+    /**
+     * This method print out the account balance.
+     */
+    @Override
     public void execute(){
-
+        double balance = account.getBalance();
+        int accountId = account.getID();
+        System.out.println("Account id: " + accountId + " has balance " + balance);
     }
 }

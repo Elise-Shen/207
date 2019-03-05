3d22d6f59ce32c3d3ca50f173d3210225e707806
maoxiaoy <xiaoyuan.mao@mail.utoronto.ca>
2019-03-05 16:59:41 -0500

    implement ChangePin

--- phase1/src/Actions/ChangePin.java
+++ phase1/src/Actions/ChangePin.java
@@ -1,6 +1,22 @@
 package Actions;
 
+import ATM.User;
+
 public class ChangePin extends UserActions {
 
+    private User user;
+    private String password;
 
+    public ChangePin(User user, String password){
+        this.user = user;
+        this.password = password;
+    }
+
+    /**
+     * This method change the password.
+     */
+    @Override
+    public void execute() {
+        user.setPassword(password);
+    }
 }

f15e4687378f2f60e49268e8270a077966020f3b
maoxiaoy <xiaoyuan.mao@mail.utoronto.ca>
2019-03-05 16:58:11 -0500

    implement RequestAccount

---
+++ phase1/src/Actions/RequestAccount.java
@@ -0,0 +1,24 @@
+package Actions;
+
+import ATM.*;
+
+public class RequestAccount extends UserActions {
+
+    private User user;
+    private BankManager bm;
+    String AccountType;
+
+    public RequestAccount(User user, BankManager bm, String AccountType){
+        this.user = user;
+        this.bm = bm;
+        this.AccountType = AccountType;
+    }
+
+    /**
+     * This method ask the BankManager to create a new account.
+     */
+    @Override
+    public void execute() {
+        bm.createAccount(user, AccountType);
+    }
+}

bb6098915abe1dad3db9d9ceac0af167c82a5ead
丁小辰 <yannading@dingxiaochendeMacBook-Air.local>
2019-03-04 23:45:38 -0500

    Added methods in all acount classes.

--- phase1/src/Accounts/Chequing.java
+++ phase1/src/Accounts/Chequing.java
@@ -1,4 +1,17 @@
 package Accounts;
+import java.util.Date;
 
 public class Chequing extends AssetAccount {
+
+    public Chequing(Date creationDate) {
+        super(creationDate);
+    }
+
+    public void decreaseBalance(double money) {
+        if (balance - money < -100) {
+            balance -= money;
+        } else {
+            System.out.println("Not enough balance!");
+        }
+    }
 }
--- phase1/src/Accounts/Saving.java
+++ phase1/src/Accounts/Saving.java
@@ -1,8 +1,13 @@
 package Accounts;
+import java.util.Date;
 
 public class Saving extends AssetAccount {
 
-    public AddInterest(){
+    public Saving(Date creationDate) {
+        super(creationDate);
+    }
 
+    public void AddInterest(){
+        balance = balance * (1 + 0.001);
     }
 }

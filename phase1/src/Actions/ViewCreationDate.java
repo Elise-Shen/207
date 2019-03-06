dc1a1d8fd991c31a17000946d03ba6d86839bd43
maoxiaoy <xiaoyuan.mao@mail.utoronto.ca>
2019-03-06 11:01:12 -0500

    make ViewCreationDate a subclass of ViewAccount to lessen duplication

--- phase1/src/Actions/ViewCreationDate.java
+++ phase1/src/Actions/ViewCreationDate.java
@@ -1,15 +1,9 @@
 package Actions;
 
-import ATM.*;
+import ATM.BankManager;
 import Accounts.Account;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
-public class ViewCreationDate extends UserActions {
-
-    private User currentUser;
-    private Account currentAccount;
+public class ViewCreationDate extends ViewAccount {
 
     public ViewCreationDate(int userID, BankManager bm){
         super(userID, bm);
@@ -21,24 +15,7 @@ public class ViewCreationDate extends UserActions {
     @Override
     public void execute() {
 
-        boolean validInput = false;
-        int accountChoice;
-        BankManager bankManager = getBankManager();
-        currentUser = bankManager.getUser(getUserID());
-        ArrayList<Account> currentUserAccounts = currentUser.getAccountList();//want to return a list of all accounts
-        while (!validInput) {
-            Scanner input = new Scanner(System.in);
-            System.out.println("\nType in the ID of the account you want to view");
-            for (Account a : currentUserAccounts) {
-                System.out.println(a.getAccountID() + " - " + a.toString());
-            }
-            accountChoice = input.nextInt();
-            currentAccount = currentUser.getAccount(accountChoice);
-            if(currentAccount != null){
-                validInput = true;
-            }else{System.out.println("Invalid Input. Please try again!");}
-        }
-
+        Account currentAccount = readCurrentAccount();
         System.out.println("\nAccount ID" + currentAccount.getAccountID() + "was created on "
                 + currentAccount.getDateOfCreation() +".");
     }

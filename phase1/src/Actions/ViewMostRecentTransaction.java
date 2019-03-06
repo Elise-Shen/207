d836f789ef4df6bc3a4fe3ac7ca13e819480d2f4
maoxiaoy <xiaoyuan.mao@mail.utoronto.ca>
2019-03-06 11:01:42 -0500

    make it a subclass of ViewAccount to lessen duplication

--- phase1/src/Actions/ViewMostRecentTransaction.java
+++ phase1/src/Actions/ViewMostRecentTransaction.java
@@ -1,13 +1,13 @@
 package Actions;
 
 import ATM.BankManager;
-import ATM.User;
 import Accounts.Account;
 
 import java.time.LocalDate;
-import java.util.*;
+import java.util.Iterator;
+import java.util.Map;
 
-public class ViewMostRecentTransaction extends UserActions {
+public class ViewMostRecentTransaction extends ViewAccount {
 
     public ViewMostRecentTransaction(int userID, BankManager bm){
         super(userID, bm);
@@ -15,26 +15,9 @@ public class ViewMostRecentTransaction extends UserActions {
 
     @Override
     public void execute() {
-        Account currentAcount = null;
-        boolean isValid = false;
-        BankManager bankManager = getBankManager();
-        User currentUser = bankManager.getUser(getUserID());
-        ArrayList<Account> currentAccounts = currentUser.getAccountList();
-        Map<LocalDate, Transactions> recent = null;
-        while(!isValid) {
-            System.out.println("\nWhich account's most recent transaction do you want to view?");
-            for (Account a : currentAccounts) {
-                Scanner input = new Scanner(System.in);
-                System.out.println(a.getAccountID() + " - " + a.toString());
-                int choice = input.nextInt();
-                if (currentUser.getAccount(choice) != null){
-                    currentAcount = currentUser.getAccount(choice);
-                    isValid = true;
-                }else{System.out.println("Invalid input. Please try again!");}
-            }
-        }
-
-        recent = currentAcount.getTransactionsList();
+        Map<LocalDate, Transactions> recent;
+        Account currentAccount = readCurrentAccount();
+        recent = currentAccount.getTransactionsList();
         Iterator<Map.Entry<LocalDate, Transactions>> entries = recent.entrySet().iterator();
         Map.Entry<LocalDate, Transactions> entry;
         LocalDate mapkey = null;

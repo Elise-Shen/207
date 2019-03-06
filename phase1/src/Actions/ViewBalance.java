6ce355a159dc991fa69be0a2b42edadac594aa16
maoxiaoy <xiaoyuan.mao@mail.utoronto.ca>
2019-03-06 11:00:21 -0500

    make ViewBalance a subclass of ViewAccount

--- phase1/src/Actions/ViewBalance.java
+++ phase1/src/Actions/ViewBalance.java
@@ -1,15 +1,9 @@
 package Actions;
 
-import ATM.*;
-import Accounts.*;
+import ATM.BankManager;
+import Accounts.Account;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
-public class ViewBalance extends UserActions {
-
-    private Account currentAccount;
-    private User currentUser;
+public class ViewBalance extends ViewAccount {
 
     public ViewBalance(int currentId, BankManager bankManager){
         super(currentId, bankManager);
@@ -20,35 +14,11 @@ public class ViewBalance extends UserActions {
      */
     @Override
     public void execute(){
-        boolean validInput = false;
-        int accountChoice = 0;
-        BankManager bankManager = getBankManager();
-        currentUser = bankManager.getUser(getUserID());
-        ArrayList<Account> currentUserAccounts = currentUser.getAccountList();//want to return a list of all accounts
-        while (!validInput) {
-            Scanner input = new Scanner(System.in);
-            System.out.println("\nType in the ID of the account you want to view");
-            for (Account a : currentUserAccounts) {
-                System.out.println(a.getAccountID() + " - " + a.toString());
-            }
-            accountChoice = input.nextInt();
-            if(currentUser.getAccount(accountChoice) != null){
-                validInput = true;
-            }else{System.out.println("Invalid Input. Please try again!");}
-        }
-
-        currentAccount = currentUser.getAccount(accountChoice);
+        Account currentAccount = readCurrentAccount();
+        int currentId = currentAccount.getAccountID();
         double balance = currentAccount.getBalance();
-        System.out.println("Account id: " + accountChoice + " has balance $" + balance);
+        System.out.println("Account id: " + currentId + " has balance $" + balance);
         System.out.println("Working");
-    }
-
-    public User getCurrentUser(){
-        return currentUser;
-    }
-
-    public Account getCurrentAccount(){
-        return currentAccount;
     }
 
 }

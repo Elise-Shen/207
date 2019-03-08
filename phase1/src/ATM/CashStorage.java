7b79223f6188483f50a7a1bd4e2592b544204468
Elise <elise.shen@mail.utoronto.ca>
2019-03-07 21:31:09 -0500

    CashStorage add checkStorage

--- phase1/src/ATM/CashStorage.java
+++ phase1/src/ATM/CashStorage.java
@@ -2,6 +2,9 @@ package ATM;
 
 /**
  * Simulates the cash storage component of the ATM
+ * 1. check if there's enough money for withdrawl
+ * 2. withdrawl money
+ * 3. check if each bill have 20
  */
 public class CashStorage {
 
@@ -12,11 +15,24 @@ public class CashStorage {
     public final int MAXSTOCK = 1000;//maximum stock of each type of bills the ATM can hold
 
     public CashStorage(){
-
         numFive = MAXSTOCK;
         numTen = MAXSTOCK;
         numTwenty = MAXSTOCK;
         numFifty = MAXSTOCK;
+    }
+    /**
+     * Step 1. Check if there;s enough money left for withdrawl
+     * checkStorage(int amount)->boolean
+     */
+    public boolean checkStorage(int amount){
+        int currentSum = numFive*5+numTen*10+numTwenty*20+numFifty*50;
+        if(currentSum>=amount)
+            return true;
+        else{
+            System.out.println("Sorry, the ATM has not enough cash right now");
+            return false;
+        }
+
     }
 
     /**
@@ -58,7 +74,8 @@ public class CashStorage {
      * @param amt
      */
     public void addBills(int type, int amt){
-
     }
+
+
 
 }

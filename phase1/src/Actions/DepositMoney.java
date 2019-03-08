20b5bdadd984d8c819b5e4a253cba3b4fef2b481
丁小辰 <yannading@dingxiaochendeMacBook-Air.local>
2019-03-07 19:04:05 -0500

    Modified DepositMoney class.

--- phase1/src/Actions/DepositMoney.java
+++ phase1/src/Actions/DepositMoney.java
@@ -48,34 +48,50 @@ public class DepositMoney extends Transactions {
             Account currentAccount = currentUser.getAccount(accountChoice);
             boolean validInput1 = false;
             while (!validInput1) {
-                BufferedReader deposit = new BufferedReader(new FileReader("Actions/desposits.txt"));
-                String line = "";
-                String previous_line = "";
-                while (line != null) {
-                    previous_line = line;
-                    line = deposit.readLine();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new FileReader("deposits.txt"));
+                    String line = "";
+                    String previous_line = "";
+                    while (line != null) {
+                        previous_line = line;
+                        line = bufferedReader.readLine();
+                        previous_line.replaceAll("\\s+", "");
+                        String[] split = previous_line.split(",");
+                        if (split[0].equalsIgnoreCase("Cash")) {
+                            int numFive = Integer.valueOf(split[1]);
+                            int numTen = Integer.valueOf(split[2]);
+                            int numTwenty = Integer.valueOf(split[3]);
+                            int numFifty = Integer.valueOf(split[4]);
+                            currentAccount.increaseBalance(numFive * 5 + numTen * 10 + numTwenty * 20 + numFifty * 50);
+                            cashStorage.addBills(5, numFive);
+                            cashStorage.addBills(10, numTen);
+                            cashStorage.addBills(20, numTwenty);
+                            cashStorage.addBills(50, numFifty);
+                            validInput1 = true;
+                        } else if (split[0].equalsIgnoreCase("Cheque")) {
+                            currentAccount.increaseBalance(Integer.valueOf(split[1]));
+                            validInput1 = true;
+                        } else {
+                            System.out.println("Invalid input.Please enter again in deposits.txt.");
+                        }
+                        bufferedReader.close();
+                    }
                 }
-                previous_line.replaceAll("\\s+", "");
-                String[] split = previous_line.split(",");
-                if (split[0].equalsIgnoreCase("Cash")) {
-                    int numFive = Integer.valueOf(split[1]);
-                    int numTen = Integer.valueOf(split[2]);
-                    int numTwenty = Integer.valueOf(split[3]);
-                    int numFifty = Integer.valueOf(split[4]);
-                    currentAccount.increaseBalance(numFive * 5 + numTen * 10 + numTwenty * 20 + numFifty * 50);
-                    cashStorage.addBills(5, numFive);
-                    cashStorage.addBills(10, numTen);
-                    cashStorage.addBills(20, numTwenty);
-                    cashStorage.addBills(50, numFifty);
-                    validInput1 = true;
-                } else if (split[0].equalsIgnoreCase("Cheque")) {
-                    currentAccount.increaseBalance(Integer.valueOf(split[1]));
-                    validInput1 = true;
-                } else {
-                    System.out.println("Invalid input.Please enter again in deposits.txt.");
+                catch(FileNotFoundException ex) {
+                    System.out.println(
+                            "Unable to open file '" +
+                                    "deposits.txt" + "'");
                 }
+                catch(IOException ex) {
+                    System.out.println(
+                            "Error reading file '"
+                                    + "deposits.txt" + "'");
+                }
+
+
+
+
             }
         }
     }
-
 }

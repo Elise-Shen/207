583d0b05665512a938cf6000a326126e8fc9b9a9
丁小辰 <yannading@dingxiaochendeMacBook-Air.local>
2019-03-09 19:32:28 -0500

    Modified the print out message in DepositMoney class so that it makes more sense.

--- phase1/src/Actions/DepositMoney.java
+++ phase1/src/Actions/DepositMoney.java
@@ -47,7 +47,8 @@ public class DepositMoney extends Transactions {
         ArrayList<Account> currentUserAccounts = currentUser.getAccountList();
         while (!validInput) {
             Scanner input = new Scanner(System.in);
-            System.out.println("\nPlease type in the ID of the account what you want to deposit money in.");
+            System.out.println("\nPlease type in the ID of the account that you want to deposit money in and \nmake sure" +
+                    " the amount to be deposited is entered in deposits.txt.");
             for (Account a : currentUserAccounts) {
                 if (a != null){
                     System.out.println(a.getAccountID() + " - " + a);
@@ -63,7 +64,6 @@ public class DepositMoney extends Transactions {
                 System.out.println("Invalid input. Please try again!");
             }
         }
-        System.out.println("Please enter things into deposits.txt");
         //currentAccount = currentUser.getAccount(accountChoice); moved up to previous if statement
         boolean validInput1 = false;
         while (!validInput1) {

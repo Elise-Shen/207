178db83e87365d0f888ef46dde27668c58155aeb
xianluca <lucas.xian@mail.utoronto.ca>
2019-03-09 20:20:30 -0500

    Added Admin Actions and relevant methods

---
+++ phase1/src/AdminActions/RestockATM.java
@@ -0,0 +1,19 @@
+package AdminActions;
+
+import ATM.BankManager;
+import ATM.CashStorage;
+
+public class RestockATM extends AdminAction {
+
+    private CashStorage cashStorage;
+
+    public RestockATM(BankManager bankManager, CashStorage cashStorage){
+        super(bankManager);
+        this.cashStorage = cashStorage;
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
---
+++ phase1/src/AdminActions/ViewUndoTransactRequests.java
@@ -0,0 +1,15 @@
+package AdminActions;
+
+import ATM.BankManager;
+
+public class ViewUndoTransactRequests extends AdminAction{
+
+    public ViewUndoTransactRequests(BankManager bankManager){
+        super(bankManager);
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}

2a3c3b3ab56b8796204351d48ee2a5e7c6d51d77
丁小辰 <yannading@dingxiaochendeMacBook-Air.local>
2019-03-09 23:41:54 -0500

    Changed PayBills class.

--- phase1/src/Actions/PayBills.java
+++ phase1/src/Actions/PayBills.java
@@ -4,6 +4,7 @@ import Accounts.*;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.*;
 
 public class PayBills extends Transactions{
     private int currentAccountID;
@@ -18,21 +19,28 @@ public class PayBills extends Transactions{
         ArrayList<Account> currentUserAccounts = bankManager.getAccountArrayList(currentUser);//want to return a list of all accounts
 
         Scanner input = new Scanner(System.in);
+        System.out.println("\nType in the ID of the account you want to use to pay bill.");
         for(Account a: currentUserAccounts){
             if (a != null) {
                 System.out.println( a.getAccountID() + " - " + a);
             }
         }
-        System.out.println("\nType in the ID of the account you want to use to pay bill");
-        // reduce the balance of the account
         int accountID = input.nextInt();
         Account account = currentUser.getAccount(accountID);
         currentAccountID = accountID;
+
+        Scanner input1 = new Scanner(System.in);
+        System.out.println("\nType in the ID of the non-user account that you want to pay the bill to.");
+        String nonUserAccountID = input1.nextLine();
+
         System.out.println("\nType in the amount of bill");
         int amount = input.nextInt();
-
+        // reduce the balance of the account
         account.decreaseBalance(amount);
-        System.out.println("A bill of $" + amount + " is paid from accountID " + accountID);
+        String result = "A bill of $" + amount + " is paid from user account " + accountID +
+                " to non-user account " + nonUserAccountID;
+        writeToOutGoingFile(result);
+        System.out.println(result);
 
     }
 
@@ -45,4 +53,26 @@ public class PayBills extends Transactions{
     public String toString() {
         return "Pay Bill";
     }
+
+    /**
+     * Write to outgoing.txt.
+     */
+    private void writeToOutGoingFile(String result) {
+        BufferedWriter writer = null;
+        try {
+            File outgoing = new File("outgoing.txt");
+
+            writer = new BufferedWriter(new FileWriter(outgoing));
+            writer.write(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // Close the writer regardless of what happens...
+                writer.close();
+            } catch (Exception e) {
+            }
+        }
+    }
 }
+

28aa5e74f95fa4f26a2fa7db3b6ea734bc7b9314
xianluca <lucas.xian@mail.utoronto.ca>
2019-03-21 22:16:59 -0400

    started to implement Currency

--- phase2/src/main/java/ATM/Main.java
+++ phase2/src/main/java/ATM/Main.java
@@ -9,18 +9,19 @@ import javax.money.Monetary;
 import javax.money.MonetaryAmount;
 import javax.money.convert.CurrencyConversion;
 import javax.money.convert.MonetaryConversions;
+import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        CurrencyUnit usd = Monetary.getCurrency("USD");
-        CurrencyUnit cad = Monetary.getCurrency("CAD");
+        CurrencyUnit usd = Monetary.getCurrency(new Locale("en", "USA"));
+        CurrencyUnit cad = Monetary.getCurrency(Locale.getDefault());
         System.out.println(usd.getDefaultFractionDigits() + " " + usd.getNumericCode());
         MonetaryAmount amt = Monetary.getDefaultAmountFactory().setCurrency(usd).setNumber(200.03).create();
 
-        Money usdMoney = Money.of(12.00, usd);
-        Money cadMoney = Money.of(15.00, cad);
+        Money usdMoney = Money.of(1.00, usd);
+        Money cadMoney = Money.of(1.00, cad);
 
         CurrencyConversion conversionCAD = MonetaryConversions.getConversion(cad);
 

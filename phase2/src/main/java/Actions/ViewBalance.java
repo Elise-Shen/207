08d62ab8108f94f04040a870bfdcd2c0a1414d23
chanho31 <willyc.chan@mail.utoronto.ca>
2019-03-25 14:08:13 -0400

    Changed Money to MonetaryAmount

--- phase2/src/main/java/Actions/ViewBalance.java
+++ phase2/src/main/java/Actions/ViewBalance.java
@@ -4,6 +4,8 @@ import ATM.BankManager;
 import Accounts.Account;
 import org.javamoney.moneta.Money;
 
+import javax.money.MonetaryAmount;
+
 public class ViewBalance extends ViewAccount {
 
     public ViewBalance(int currentId, BankManager bankManager){
@@ -17,7 +19,7 @@ public class ViewBalance extends ViewAccount {
     public void execute(){
         Account currentAccount = readCurrentAccount();
         int currentId = currentAccount.getAccountID();
-        Money currencyBalance = currentAccount.getCurrencyBalance();
+        MonetaryAmount currencyBalance = currentAccount.getCurrencyBalance();
         System.out.println("Account id: " + currentId + " has balance of " + currencyBalance);
     }
 }

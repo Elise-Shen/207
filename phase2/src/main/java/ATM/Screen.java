3d4c179f15c62e68686112261267e695aabfcc7a
maoxiaoy <xiaoyuan,mao@mail.utoronto.ca>
2019-03-25 22:50:17 -0400

    Fixed the frame

--- phase2/src/main/java/ATM/Screen.java
+++ phase2/src/main/java/ATM/Screen.java
@@ -5,24 +5,37 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class Screen {
-    private JFrame f;
-    private JButton exit;
+public class Screen extends JFrame{
+
+    private static final String TITLE = "ATM";
+
+    private JButton buttonExit;
+    private JLabel label;
 
     private Screen(){
-        f = new JFrame("ATM");
-        f.setBounds(300, 200, 600, 500);
-        f.setLayout(null);
-        f.setVisible(true);
-        f.addWindowListener(new WindowAdapter() {
+
+        createView();
+
+        setTitle(TITLE);
+        setBounds(300, 200, 600, 500);
+        setLayout(null);
+        setLocationRelativeTo(null);
+        addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
-                System.exit(0);
                 System.out.println("Exit the program.");
+                System.exit(0);
             }
         });
+        add(buttonExit);
+        setResizable(false);
 
-        exit = new JButton("Exit");
-        exit.addActionListener(new ActionListener() {
+        run();
+    }
+
+    private void createView(){
+
+        buttonExit = new JButton("Exit");
+        buttonExit.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -30,11 +43,8 @@ public class Screen {
                 System.exit(0);
             }
         });
-        exit.setLocation(400, 400);
-        exit.setSize(100, 25);
-        f.add(exit);
-
-        run();
+        buttonExit.setLocation(400, 400);
+        buttonExit.setSize(100, 25);
     }
 
     private void run(){
@@ -43,23 +53,26 @@ public class Screen {
     }
 
     private void startUpPage(){
-        JLabel label = new JLabel("ATM starting Up ...");
+        label = new JLabel("ATM starting Up ...");
         label.setLocation(200,0);
-        label.setSize(100, 50);
-        f.add(label);
-        Timer timer = new Timer(5000, new ActionListener() {
+        label.setSize(150, 50);
+        add(label);
+        Timer timer = new Timer(3000, new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
+                System.out.println("ATM starting up ...");
                 label.setVisible(false);
+
             }
         });
+        timer.setRepeats(false);
         timer.start();
     }
 
-    public void loginPage(){
+    private void loginPage(){
 
     }
 
-    public static void main(String[] args) { new Screen(); }
+    public static void main(String[] args) { new Screen().setVisible(true);; }
 }

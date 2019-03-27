396012d90baf7f5f6c9cb5e151e8f8a688e6aa40
Elise <elise.shen@mail.utoronto.ca>
2019-03-26 22:08:09 -0400

    BankProduct-product factory

--- phase2/src/main/java/BankProducts/ProductFactory.java
+++ phase2/src/main/java/BankProducts/ProductFactory.java
@@ -1,7 +1,19 @@
 package BankProducts;
-import Accounts.*;
+
 
 import java.io.Serializable;
-public class ProductFactory implements Serializable  {
+public class ProductFactory implements Serializable {
+    public BankProduct getBankProduct(int productType, int amount, int months) {
+        if (productType == 1) {
+            return new LongTermMortgage(amount, months);
+        } else if (productType == 2) {
+            return new ShortTermMortgage(amount, months);
+        } else if (productType == 3) {
+            return new HighRiskInvestment(amount);
+        } else if (productType == 4) {
+            return new LowRiskInvestment(amount);
+        }
+        return null;
 
+    }
 }

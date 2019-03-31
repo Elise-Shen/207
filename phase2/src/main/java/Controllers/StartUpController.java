72f12f340cb42533cf78811afe380d494953a4a8
丁小辰 <yannading@dingxiaochendeMacBook-Air.local>
2019-03-31 13:29:05 -0400

    Fixed time issue.

--- phase2/src/main/java/Controllers/StartUpController.java
+++ phase2/src/main/java/Controllers/StartUpController.java
@@ -2,28 +2,22 @@ package Controllers;
 
 
 
+import ATM.ATM_Machine;
 import ATM.Main;
-import Controllers.Helpers.ConfirmBoxController;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
+
 import javafx.util.Duration;
 
-import java.awt.event.ActionEvent;
-import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
@@ -46,10 +40,20 @@ public class StartUpController implements Initializable {
         Main.showNewBorderPane("/BankManagerLoginPage.fxml");
     }
 
-    private void initClock(){
+    private void initClock() {
         Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
             dateTimeLabel.setText(LocalDateTime.now().format(formatter));
+            ATM_Machine atm = Main.getCurrentATM();
+
+            try {
+                if (LocalTime.now() == LocalTime.MIDNIGHT) {
+                    Thread.sleep(300);
+                }
+            } catch (Exception e1) {
+                //
+            }
+            atm.addInterests();
         }), new KeyFrame(Duration.seconds(1)));
         clock.setCycleCount(Animation.INDEFINITE);
         clock.play();

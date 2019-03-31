912372d98244e046e309cb97ed479044fc5431f8
maoxiaoy <xiaoyuan.mao@mail.utoronto.ca>
2019-03-31 03:26:00 -0400

    update

---
+++ phase2/src/main/java/Controllers/UserActionControllers/BankProductController.java
@@ -0,0 +1,13 @@
+package Controllers.UserActionControllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BankProductController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}

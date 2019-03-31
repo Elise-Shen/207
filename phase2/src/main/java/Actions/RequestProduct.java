c2b14116191dc7d272ca9f47f985ffb434d484e7
Elise <elise.shen@mail.utoronto.ca>
2019-03-30 22:57:05 -0400

    Request Product update

--- phase2/src/main/java/Actions/RequestProduct.java
+++ phase2/src/main/java/Actions/RequestProduct.java
@@ -2,20 +2,34 @@ package Actions;
 
 import ATM.BankProductsEmployee;
 import ATM.Keypad;
+import ATM.User;
+
+import java.time.LocalTime;
 
 public class RequestProduct extends UserActions {
+    private static final int LIMIT = 1;
     public RequestProduct(int userID, BankProductsEmployee bpe, int accountID){
         super(userID,bpe,accountID);
     }
     @Override
     public void execute(){
         BankProductsEmployee bankProductsEmployee = getBankProductsEmployee();
-        int productType = getProductType();
-        int productAmount = getProductAmount();
-        int puductLength = getProductLength(productType);
-        bankProductsEmployee.requestProducts(getUserID(),getAccountID(),productType,productAmount,puductLength);
+        User currentUser = bankProductsEmployee.getUser(getUserID());
+        LocalTime currentTime = LocalTime.now();
+        LocalTime countResetTime = LocalTime.MIDNIGHT;
+        if(currentTime == countResetTime){
+            currentUser.resetCount();
+        }
+        if(currentUser.getCount() < LIMIT ) {
+
+            int productType = getProductType();
+            int productAmount = getProductAmount();
+            int puductLength = getProductLength(productType);
+            bankProductsEmployee.requestProducts(getUserID(),getAccountID(),productType,productAmount,puductLength);
+        }else {System.out.println("Reached Daily Account Request Limit");}
     }
     public int getProductLength(int type){
+
         int length = 0;
         if (type==1 || type==2){
             Keypad keypad = new Keypad();

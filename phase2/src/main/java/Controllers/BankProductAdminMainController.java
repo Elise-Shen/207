ca9955f9dd3a24ab9fa37fae865393f7482eb0f5
maoxiaoy <xiaoyuan.mao@mail.utoronto.ca>
2019-03-31 13:24:45 -0400

    back product admin main page

--- phase2/src/main/java/Controllers/BankProductAdminMainController.java
+++ phase2/src/main/java/Controllers/BankProductAdminMainController.java
@@ -1,4 +1,23 @@
 package Controllers;
 
-public class BankProductAdminMainController {
+import ATM.Main;
+import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BankProductAdminMainController implements Initializable {
+
+    public void goBack() throws Exception{
+        Main.showNewBorderPane("/BankEmployeeMainPage.fxml");
+    }
+
+    public void viewProductButton() throws  Exception{
+        Main.showNewBorderPane("/AdminActionResources/ViewProductRequest.fxml");
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }

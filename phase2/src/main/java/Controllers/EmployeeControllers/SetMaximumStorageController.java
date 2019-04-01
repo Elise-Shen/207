9ef8d020b9720ab2aebe083f742fe09af94a5063
dingyann <yanna.ding@mail.utoronto.ca>
2019-03-31 20:40:59 -0400

    Added SetMaxStock functionality.

--- phase2/src/main/java/Controllers/EmployeeControllers/SetMaximumStorageController.java
+++ phase2/src/main/java/Controllers/EmployeeControllers/SetMaximumStorageController.java
@@ -1,33 +1,67 @@
 package Controllers.EmployeeControllers;
 
-import ATM.ATM_Machine;
-import ATM.CashStorage;
-import ATM.Main;
+import ATM.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import javax.money.CurrencyUnit;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Map;
+import java.util.Iterator;
 
 public class SetMaximumStorageController implements Initializable {
 
     @FXML
-    private Label currentMaxStock;
+    private ComboBox<String> selectCurrency;
+
+    @FXML
+    private Label result;
 
     @FXML
     private TextField text;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        CashStorage cs = Main.getCurrentATM().getCashStorage();
-
+        ObservableList<String> currencies = FXCollections.observableArrayList();
+        Iterator<Map.Entry<String, CurrencyStorage>> entries = Main.getCurrentATM().getCashStorage().
+                getCashStorage().entrySet().iterator();
+        Map.Entry<String, CurrencyStorage> entry;
+        String mapKey;
+        while (entries.hasNext()) {
+            entry = entries.next();
+            mapKey = entry.getKey();
+            currencies.add(mapKey);
+        }
+        selectCurrency.setItems(currencies);
     }
 
     public void goBack() throws Exception {
         text.clear();
         Main.showNewBorderPane("/EmployeeActionPage.fxml");
+    }
+
+    public void okPressed() {
+        String currencyUnit = selectCurrency.getValue();
+        try {
+            int newMaxStock = Integer.parseInt(text.getText());
+            if (newMaxStock >= 0) {
+                CurrencyStorage cs = Main.getCurrentATM().getCashStorage().getCashStorage().get(currencyUnit);
+                int oldMaxStock = cs.getMaxStock();
+                cs.setMaxStock(newMaxStock);
+                result.setText("The maximum stock for any cash type in " + currencyUnit + " was changed from " +
+                        oldMaxStock + " to " + newMaxStock);
+            } else {
+                result.setText("Invalid input.");
+            }
+        } catch (Exception e) {
+            //
+        }
     }
 
 
